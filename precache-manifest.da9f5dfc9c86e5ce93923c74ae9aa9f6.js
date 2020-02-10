12deecf711cb3ff1101c69eedc8900ac1b1a5aca
ArtemSpiridovich <ArtemSpiridovich99@gmail.com>
2020-02-10 16:31:40 +0300

    Updates

---
+++ precache-manifest.da9f5dfc9c86e5ce93923c74ae9aa9f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38a67b1e6b04860fad4cbf18a8bd364",
+    "url": "/game-dog/index.html"
+  },
+  {
+    "revision": "688b5a2a41058c8532a5",
+    "url": "/game-dog/static/css/main.d509e602.chunk.css"
+  },
+  {
+    "revision": "51556e054d1c1a0097ca",
+    "url": "/game-dog/static/js/2.c00d48ca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/game-dog/static/js/2.c00d48ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "688b5a2a41058c8532a5",
+    "url": "/game-dog/static/js/main.dfc9a5ca.chunk.js"
+  },
+  {
+    "revision": "c7b8c2f7ff6430114bf5",
+    "url": "/game-dog/static/js/runtime-main.bb53cfbd.js"
+  },
+  {
+    "revision": "75fa68d4d95a95385dabee5a76f50797",
+    "url": "/game-dog/static/media/dog.75fa68d4.mp3"
+  }
+]);
